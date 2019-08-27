16aa2d9c85f30a54b2011e302e82b82c03bc16f1
neod123 <54597838+neod123@users.noreply.github.com>
2019-08-27 20:46:30 +0200

    Create nextcloud_installer.sh

---
+++ nextcloud_installer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# wget /gogs_installer.sh | sh gogs_installer.sh |  rm  gogs_installer.sh
+
+
+1. install/check nginx
+
+2. install nextcloud
+
+3. install/check php7.3
+
+4. install/check mariadb
+
+5. setup domain name
+
+6. setup certificat
+
+7. Optimisation
+7.1 timeout
+7.2 cache PHP
+7.3 Redis
+
