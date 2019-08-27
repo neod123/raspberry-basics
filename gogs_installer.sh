0746083b9a2c4dc9d4fcc345efd0685b5b24e154
neod123 <54597838+neod123@users.noreply.github.com>
2019-08-27 22:54:47 +0200

    Update gogs_installer.sh

--- gogs_installer.sh
+++ gogs_installer.sh
@@ -30,7 +30,7 @@ sudo apt-get install git -y
 
 echo "4.==> install gogs"
 cd /opt
-sudo wget https://dl.gogs.io/0.11.53/gogs_0.11.91_raspi_armv7.zip
+sudo wget https://dl.gogs.io/0.11.91/gogs_0.11.91_raspi_armv7.zip
 sudo unzip gogs_0.11.91_raspi_armv7.zip
 sudo rm gogs_0.11.91_raspi_armv7.zip
 
