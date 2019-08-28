422fdcb59d7cfd8d170df196845845c302ff20e5
neod123 <54597838+neod123@users.noreply.github.com>
2019-08-28 22:18:25 +0200

    Create gogs_database.sql

---
+++ gogs_config/gogs_database.sql
@@ -0,0 +1,7 @@
+SET GLOBAL innodb_file_per_table = ON;
+CREATE DATABASE gogsdb;
+CREATE USER 'gogsuser'@'localhost' IDENTIFIED BY 'new_password_here';
+GRANT ALL ON gogsdb.* TO 'gogsuser'@'localhost' IDENTIFIED BY 'user_password_here' WITH GRANT OPTION;
+ALTER DATABASE gogsdb CHARACTER SET = utf8mb4 COLLATE utf8mb4_unicode_ci;
+FLUSH PRIVILEGES;
+EXIT;
