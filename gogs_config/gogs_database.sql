3acc626f5343912dd54a7f60894b31696a0f5d20
neod123 <54597838+neod123@users.noreply.github.com>
2019-08-31 13:36:33 +0200

    Update gogs_database.sql

--- gogs_config/gogs_database.sql
+++ gogs_config/gogs_database.sql
@@ -1,3 +1,8 @@
-GRANT ALL ON gogs.* TO 'gogs'@'localhost' IDENTIFIED BY 'm_password';
+SET GLOBAL innodb_file_per_table = ON;
+CREATE DATABASE gogsdb;
+CREATE USER 'gogsuser' IDENTIFIED BY 'm_password';
+GRANT ALL ON gogsdb.* TO 'gogsuser'@'localhost' IDENTIFIED BY 'm_password' WITH GRANT OPTION;
+ALTER DATABASE gogsdb CHARACTER SET = utf8mb4 COLLATE utf8mb4_unicode_ci;
 FLUSH PRIVILEGES;
-exit
+SHOW DATABASEs;
+select user, password, host from mysql.user;
