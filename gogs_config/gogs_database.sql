af34cb76424c639aa09cd518fd21ac26b336b734
neod123 <54597838+neod123@users.noreply.github.com>
2019-08-31 13:11:51 +0200

    Update gogs_database.sql

--- gogs_config/gogs_database.sql
+++ gogs_config/gogs_database.sql
@@ -1,9 +1,3 @@
-CREATE DATABASE IF NOT EXISTS gogsdb CHARACTER SET utf8 COLLATE utf8_general_ci;
-grant usage on gogsdb.* to gogs@localhost identified by 'm_password';
-grant all privileges on gogsdb.* to gogs@localhost ;
+GRANT ALL ON gogs.* TO 'gogs'@'localhost' IDENTIFIED BY 'm_password';
 FLUSH PRIVILEGES;
-use gogsdb;
-SET default_storage_engine=INNODB;
-SHOW DATABASES;
-
-
+exit
