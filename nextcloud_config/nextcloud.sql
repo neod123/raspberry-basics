660b87744d575b7f1262e50fe3752045e69a8c67
neod123 <54597838+neod123@users.noreply.github.com>
2019-10-01 19:53:11 +0200

    Update nextcloud.sql

--- nextcloud_config/nextcloud.sql
+++ nextcloud_config/nextcloud.sql
@@ -2,7 +2,7 @@ SET GLOBAL innodb_file_per_table = ON;
 CREATE DATABASE nextcloud;
 CREATE USER 'nextcloud' IDENTIFIED BY 'm_password';
 GRANT ALL ON nextcloud.* TO 'nextcloud'@'localhost' IDENTIFIED BY 'm_password' WITH GRANT OPTION;
-ALTER DATABASE gogsdb CHARACTER SET = utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE nextcloud CHARACTER SET = utf8mb4 COLLATE utf8mb4_unicode_ci;
 FLUSH PRIVILEGES;
 SHOW DATABASES;
 select user, password, host from mysql.user;
